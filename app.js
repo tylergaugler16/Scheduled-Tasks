6202be2816325ca31f6446be659eafcc4d530d19
tylergaugler16 <tylergaugler16@gmail.com>
2018-02-27 20:42:12 -0500

    changed port

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
+
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
+
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
