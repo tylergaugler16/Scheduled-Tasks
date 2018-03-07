b7a295c95399ffc22068a65161322c42502f8d4d
tylergaugler16 <tylergaugler16@gmail.com>
2018-03-06 21:12:33 -0500

    syntax error

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var app = express();
 // }
 
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.listen(port, ip, function () {
   console.log( "Listening on " + ip + ", port " + port );
