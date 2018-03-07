4b9ac78fd801902342c8f0a0196257716fc1d3bd
tylergaugler16 <tylergaugler16@gmail.com>
2018-03-06 21:10:52 -0500

    added message

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
 
 app.listen(port, ip, function () {
-  console.log( "Listening on " + ip + ", port " + port )
+  console.log( "Listening on " + ip + ", port " + port );
+  console.log("\n please work!")
 });
 
 app.get('/', function (req, res) {
