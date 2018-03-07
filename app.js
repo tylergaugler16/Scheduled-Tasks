f5ae0f50f61d33a000346748f872ed72a4033f65
tylergaugler16 <tylergaugler16@gmail.com>
2018-03-06 21:05:04 -0500

    change ip/port again

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var app = express();
 
-var db_name = "sampledb"
+// var db_name = "sampledb"
 //provide a sensible default for local development
-mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+// mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
 //take advantage of openshift env vars when available:
-if(process.env.OPENSHIFT_MONGODB_DB_URL){
-  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
-}
+// if(process.env.OPENSHIFT_MONGODB_DB_URL){
+//   mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+// }
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+
+app.listen(port, ip, function () {
+  console.log( "Listening on " + ip + ", port " + port )
 });
 
 app.get('/', function (req, res) {
